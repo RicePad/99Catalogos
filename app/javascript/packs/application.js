1fddd77f9c3ef40f89f033073e3b9d9970b35aca
JONATHAN <jonn.thinkrtc@gmail.com>
2017-12-11 19:57:34 -0800

    first commit and configuration

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../components/App';
+
+const showComponent = document.querySelector('.pin-index')
+
+ReactDOM.render(
+	<App />, showComponent
+	)
