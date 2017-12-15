294e5755e5d285afafdb95f2f55aec7ded2cfc6c
JONATHAN <jonn.thinkrtc@gmail.com>
2017-12-14 17:44:57 -0800

    Integrated react dependadices and created GigIndex component

---
+++ app/javascript/components/GigIndex.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GigIndex extends Component {
+	
+	render(){
+		return(
+			<div>Gig Index Component</div>
+			)
+	}
+}
+
+
+export default GigIndex;
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../components/App';
+import GigIndex from '../components/GigIndex';
+import { BrowserRouter, Router, Route, Switch } from 'react-router';
+
+// Redux Setup
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import reducers from '../reducers'
 
 const showComponent = document.querySelector('.pin-index')
 
 ReactDOM.render(
-	<App />, showComponent
+	<GigIndex />, showComponent
 	)
