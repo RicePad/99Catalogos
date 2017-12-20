22b6f79d2e2ca2599bebd5b0f8c66f842881a200
JON <jonn.thinkrtc@gmail.com>
2017-12-20 15:28:19 -0800

    testing deployment to production after new updateds

--- app/javascript/actions/index.js
+++ app/javascript/actions/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 export const FETCH_PINS = "FETCH_PINS";
 
-const ROOT_URL = "http://localhost:5000/api/v1/pins"
-// const ROOT_URL = "https://rice-printerest.herokuapp.com/api/v1/pins.json"
+// const ROOT_URL = "http://localhost:5000/api/v1/pins"
+const ROOT_URL = "https://rice-printerest.herokuapp.com/api/v1/pins.json"
 
 
 
