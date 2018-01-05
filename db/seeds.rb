b85dfa8af535abd31b4f98017c12ea72d6bd0515
JON <jonn.thinkrtc@gmail.com>
2018-01-04 19:24:27 -0800

    fix footer style and updated demo login

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-10.times do |pin|
-	pin = Pin.create(title: "Awesome Pin", description: "greatest pin", thumb_image: "https://s3-us-west-1.amazonaws.com/mitiendashop/uploads/pin/main_image/79/Rollback.png", main_image: "https://s3-us-west-1.amazonaws.com/mitiendashop/uploads/pin/main_image/79/Rollback.png", user_id: '1')
+1.times do | user |
+	user = User.create(email: 'admin@example.com', password: '123456', password_confirmation: '123456')
+	user.save
 end
 
+puts '1 user has beeen created'
 
+ 5.times do |pin|
+	@pin = Pin.create(title: "Awesome Pin", description: "greatest pin", thumb_image: "https://s3-us-west-1.amazonaws.com/mitiendashop/uploads/pin/main_image/79/Rollback.png", main_image: "https://s3-us-west-1.amazonaws.com/mitiendashop/uploads/pin/main_image/79/Rollback.png", user_id: "1")
+ end
 
 puts "10 pins created!"
